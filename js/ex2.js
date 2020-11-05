6c49c208ec7f2059aa5b1ad75b4e2317161c3d6e
unknown <you@example.com>
2020-11-05 16:37:41 -0300

    Segundo Commit

---
+++ js/ex2.js
@@ -0,0 +1,28 @@
+document.querySelector('#btn').addEventListener('click', () => {
+    let n1, n2,totala,totals,totalm,totald;
+    /* todas as vezes que a informação vier de um input ela vem como se fosse uma string '1' */
+    n1=parseInt(document.getElementById('n1').value);
+    n2=parseInt(document.getElementById('n2').value);
+    
+    totala= n1+n2;
+    totals= n1-n2;
+    totalm= n1*n2;
+     
+    
+    if (n2 == 0) {
+        totald = 'Impossível dividir por zero!'
+    } else{
+        totald= n1/n2;
+    }
+    
+    /* lebrar que quebra de linha é '\n' */
+    alert('Resultado da adição: '+totala+'\n' +
+    'Resultado da subtração: '+totals+'\n'+
+    'Resultado da multiplicação: '+totalm+'\n'+
+    'Resultado da divisão: '+totald);
+
+    /* NaN - not a number = não é um número */
+    /* acrescentar os cálculos de subtração n1 - n2, multiplicação n1*n2 e a divisão, porém tome cuidado pois o usuário pode digitar 0 e eu não posso dividir por 0.
+    n1/n2 se n2>0
+    Mostrar os resultados no mesmo alert, somente quebrando a linha */
+})
---
+++ js/ex3.js
@@ -0,0 +1,29 @@
+document.querySelector('#btnpar').addEventListener('click', () =>{
+/* arrow function */
+/* declarar variável e atribuir */
+let num=parseInt(document.getElementById('num').value)
+
+/* dividir o númerlo por 2 e verificar o resto */
+resto = num%2;
+if(resto==0)
+    alert('O número é par!');
+else
+    alert('O número é ímpar')
+})
+
+/* outra função::::::::: */
+document.querySelector('#btnano').addEventListener('click', ()=>{
+let ano=parseInt(document.getElementById('ano').value);
+let resto;
+/* 
+= atribuição
+== comparação de valor
+=== comparação de valor e tipo (string,int...) */   
+resto = ano%4;
+if(resto==0){
+    alert('O ano é bissexto!');
+}
+else {
+    alert('O ano não é bissexto!')
+}
+})
